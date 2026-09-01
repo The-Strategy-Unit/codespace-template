9b802c1e0223f68a6ef4448f2c37905bda04fca9
Hon YiWen (MLCSU) <yiwen.hon1@nhs.net>
2026-09-01 11:31:11 +0100

    improve r container

--- scripts/timer.py
+++
@@ -1,8 +0,0 @@
-import time
-
-i = 0
-minutes = 5
-while True:
-    print("it's been", i, "minute since this started. Thank you for your patience!")
-    i = i + minutes
-    time.sleep(minutes * 60)
